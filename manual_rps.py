d3822111cca651db74f499cbc98e0a0ec6e2f6c8
CMD97 <connhardcastle97@gmail.com>
2022-12-13 22:53:55 +0000

    final manual game

--- manual_rps.py
+++ manual_rps.py
@@ -1,69 +1,69 @@
 import random
 
+class RPS:
+    def __init__(self, options, games_played=0, wins=0, losses=0, ties=0):
 
-options = ['Rock', 'Paper', 'Scissors']
-wins = 0
-losses = 0
-ties = 0
-games_played = 0
+        self.computer_choice = random.choice(options)
+        self.wins = wins
+        self.losses = losses
+        self.ties = ties
+        self.games_played = games_played
 
-def welcome():
-    print('Welcome to Rock, Paper, Scissors! This game is a best of 5.\n')
+        print('Welcome to Rock, Paper, Scissors! This game is a best of 5.\n')
 
-def get_user_choice():
-    while True:
-        user_choice = input(f'Choose either {options[0]}, {options[1]} or {options[2]}: ').title()
-        if user_choice != 'Rock' and user_choice != 'Paper' and user_choice != 'Scissors':
-            print(f'{user_choice} is not a valid option. Please make sure to type {options[0]}, {options[1]} or {options[2]}.')
+    def get_user_choice(self):
+        while True:
+            user_choice = input(f'Choose either {options[0]}, {options[1]} or {options[2]}: ').title()
+            if user_choice != 'Rock' and user_choice != 'Paper' and user_choice != 'Scissors':
+                print(f'{user_choice} is not a valid option. Please make sure to type {options[0]}, {options[1]} or {options[2]}. \n')
+            else:
+                print(f'You chose: {user_choice}')
+                return user_choice
+                
+    def get_computer_choice(computer_choice, options):
+        computer_choice = random.choice(options)
+        print(f'Computer chose:', computer_choice)
+        return computer_choice
+
+    def get_winner(self, user_choice, computer_choice):
+        global wins, losses, ties, games_played
+        if user_choice == computer_choice:
+            print('You both chose the same, it\'s a tie!')
+            self.ties += 1
+        elif user_choice == 'Rock' and computer_choice == 'Scissors' or user_choice == 'Scissors' and computer_choice == 'Paper' or user_choice == 'Paper' and computer_choice == 'Rock':
+            print(f'{user_choice} beats {computer_choice}, you win!')
+            self.wins += 1
+            self.games_played += 1
         else:
-            print(f'You chose: {user_choice}')
-            return user_choice
-            
-def get_computer_choice():
-    computer_choice = random.choice(options)
-    print(f'Computer chose:', computer_choice)
-    return computer_choice
+            print(f'{user_choice} gets beat by {computer_choice}, you lose!')
+            self.losses += 1
+            self.games_played += 1
+        print(f'''
+    wins:   {self.wins}
+    losses: {self.losses}
+    ties:   {self.ties}
+    ''')
 
-def get_winner():
-    global wins, losses, ties, games_played
-    user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
-    if user_choice == computer_choice:
-        print('You both chose the same, it\'s a tie!')
-        ties += 1
-    elif user_choice == 'Rock' and computer_choice == 'Scissors' or user_choice == 'Scissors' and computer_choice == 'Paper' or user_choice == 'Paper' and computer_choice == 'Rock':
-        print(f'{user_choice} beats {computer_choice}, you win!')
-        wins += 1
-        games_played += 1
-    else:
-        print(f'{user_choice} gets beat by {computer_choice}, you lose!')
-        losses += 1
-        games_played += 1
-    print(f'''
-wins:   {wins}
-losses: {losses}
-ties:   {ties}
-''')
-
-def play():
-    welcome()
+def play(options):
+    game = RPS(options, games_played=0)
     while True:
-        if games_played != 5 and wins != 3 and losses != 3:
-            get_winner()
-        elif wins == 3 and losses == 1:
-            print(f'You won! {wins} wins over the computers {losses} win. \n')
+        if game.games_played != 5 and game.wins != 3 and game.losses != 3:
+            user_choice = game.get_user_choice()
+            computer_choice = game.get_computer_choice(options)
+            game.get_winner(user_choice, computer_choice)
+        elif game.wins == 3 and game.losses == 1:
+            print(f'You won! {game.wins} wins over the computers {game.losses} win. \n')
+            break
+        elif game.wins == 3 and (game.losses == 2 or game.losses == 0):
+            print(f'You won! {game.wins} wins over the computers {game.losses} wins. \n')
             play_again()
             break
-        elif wins == 3 and (losses == 2 or losses == 0):
-            print(f'You won! {wins} wins over the computers {losses} wins. \n')
-            play_again()
-            break
-        elif (wins == 2 or wins == 0) and losses == 3:
-            print(f'You lost! {wins} wins over the computers {losses} wins. \n')
+        elif (game.wins == 2 or game.wins == 0) and game.losses == 3:
+            print(f'You lost! {game.wins} wins over the computers {game.losses} wins. \n')
             play_again()
             break
         else:
-            print(f'You lost! Only {wins} win over the computers {losses} wins.\n')
+            print(f'You lost! Only {game.wins} win over the computers {game.losses} wins.\n')
             play_again()
             break
 
@@ -80,7 +80,7 @@ def play_again():
                 print('Let\'s jump back in!')
                 print('_______________________________________________________')
                 print('\n')
-                play()
+                play(options)
                 break
             elif ask_user_to_play_again == 'N':
                 print('\n')
@@ -90,4 +90,6 @@ def play_again():
             else: 
                 print('Invalid selection, please choose either Y or N')
 
-play()
+if __name__ == '__main__':
+    options = ['Rock', 'Paper', 'Scissors']
+    play(options)
